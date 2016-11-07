dff4419cd8a60587c5e6f08505d2c26f4db44126
Dane Harrigan <dane@heroku.com>
2016-11-07 15:13:22 -0800

    fix status codes and headers

--- cmd/cgc-web/main.go
+++ cmd/cgc-web/main.go
@@ -15,7 +15,8 @@ import (
 
 var (
 	BungieAPIKey   = os.Getenv("BUNGIE_API_KEY")
-	ErrInvalidFrom = errors.New("invalid from parameter")
+	ErrInvalidForm = errors.New("invalid form parameter")
+	ErrInvalidURL  = errors.New("invalid URL")
 )
 
 func init() {
@@ -48,7 +49,7 @@ func NewAPI() *API {
 }
 
 type Message struct {
-	message string `json:"message"`
+	Message string `json:"message"`
 }
 
 type API struct {
@@ -68,11 +69,10 @@ func (api *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (api *API) HandleMessage(statusCode int, message string, w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(statusCode)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
 
-	msg := &Message{message}
-	err := json.NewEncoder(w).Encode(msg)
+	err := json.NewEncoder(w).Encode(Message{message})
 	if err != nil {
 		log.Printf("at=HandleMessage fn=Encode error=%q", err)
 	}
@@ -90,12 +90,17 @@ func (api *API) HandleGames(w http.ResponseWriter, r *http.Request) {
 		var err error
 		from, err = time.Parse(counter.TimeFormat, fromParam)
 		if err != nil {
-			api.HandleMessage(400, ErrInvalidFrom.Error(), w, r)
+			api.HandleMessage(400, ErrInvalidForm.Error(), w, r)
 			return
 		}
 	}
 
 	params := api.pattern.FindStringSubmatch(r.URL.Path)
+	if len(params) != 4 || missingValues(params) {
+		api.HandleMessage(400, ErrInvalidURL.Error(), w, r)
+		return
+	}
+
 	counter := counter.New(BungieAPIKey, params[1], params[2], params[3])
 	results, err := counter.GetResults(from, to)
 	if err != nil {
@@ -103,8 +108,9 @@ func (api *API) HandleGames(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+
 	if err := json.NewEncoder(w).Encode(results); err != nil {
 		log.Printf("at=HandleGames fn=Encode error=%q", err)
 	}
@@ -115,4 +121,14 @@ func (api *API) HandleRecover(w http.ResponseWriter, r *http.Request) {
 		log.Printf("at=HandleRecover error=%q", err)
 		api.HandleMessage(500, http.StatusText(500), w, r)
 	}
+}
+
+func missingValues(values []string) bool {
+	for _, v := range values {
+		if v == "" {
+			return true
+		}
+	}
+
+	return false
 }
